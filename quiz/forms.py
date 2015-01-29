d65015f27e176ea127b1149399592a59a5098809
Adam Babik <adam.babik@10clouds.com>
2015-01-29 14:23:10 +0100

    Styles

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,9 +1,12 @@
+from django import forms
 from django.forms import ModelForm
 
 from .models import Answer
 
 
 class AnswerForm(ModelForm):
+    value = forms.CharField(label='Your Answer')
+
     class Meta:
         model = Answer
         fields = ('value', )
