ad48635d8aa30c78fd44d6785574375aca082561
Adam Babik <adam.babik@10clouds.com>
2015-01-29 14:39:08 +0100

    Layout fixes

--- meetjs/settings.py
+++ meetjs/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'meetjs.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'meetjs',
+        'USER': 'meetjs',
+        'PASSWORD': 'meetjs',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -94,9 +98,9 @@ USE_L10N = True
 USE_TZ = True
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
